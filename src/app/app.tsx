41cff995b74b11dd53fc2da66d67f2c9dae00f23
JamesHensman1 <jameshensman123@gmail.com>
2023-10-08 11:30:09 +0100

    New Boilerplate
    Signed-off-by: JamesHensman1 <jameshensman123@gmail.com>

---
+++ src/app/app.tsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+export function App() {
+  return (
+    <div>
+      <h1> Notes App</h1>
+    </div>
+  );
+}
+
+export default App;
