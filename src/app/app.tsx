6246a75b22938f602ee79dc48dcf579f13bada80
JamesHensman1 <jameshensman123@gmail.com>
2023-10-08 11:54:33 +0100

    Add notes
    Signed-off-by: JamesHensman1 <jameshensman123@gmail.com>

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,10 +1,36 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
+import { useState } from 'react'
 
 export function App() {
+  const [notes, setNotes] = useState([{title: 'Note 1', content: 'Make notes app'}]);
+  const [inputTitle, setInputTitle] = useState("");
+  const [inputContent, setInputContent] = useState("");
   return (
     <div>
       <h1> Notes App</h1>
+      <div>
+      {
+        notes.map(note => (
+          <div>
+            <h2>{note.title}</h2>
+            <p>{note.content}</p>
+          </div>
+        
+        ))
+      }
+      </div>
+      <div>
+        <input placeholder='Title' onChange={e => setInputTitle(e.target.value)} value={inputTitle}></input>
+        <input placeholder='Content' onChange={e => setInputContent(e.target.value)} value={inputContent}></input>
+        <button onClick={
+          () => {
+              setNotes([...notes, {title:inputTitle, content:inputContent}])
+              setInputTitle("")
+              setInputContent("")
+            }
+          }>Add Note</button>
+      </div>
+      
     </div>
   );
 }
