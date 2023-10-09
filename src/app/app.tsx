7089989ffa710a41f584a46ad0f6244d7c055ca7
Stephen Ingham <smi505@york.ac.uk>
2023-10-10 00:50:26 +0100

    created starter branch with functionality removed

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -27,8 +27,8 @@ function Note({note, idx, del, edit} : NoteInput) {
         <p className={styles.note_content}>{note.content}</p>
       </div>
       <div className={styles.note_buttons}>
-        <button onClick={() => del(idx)}>Delete</button>
-        <button onClick={() => edit(idx)}>Edit</button>
+        <button /* onClick={() => del(idx)} */>Delete</button>
+        <button /* onClick={() => edit(idx)} */>Edit</button>
       </div>
   </div>
   )
@@ -42,24 +42,24 @@ function EditableNote({note, idx, save}: NoteEditInput) {
     <input className={styles.title} placeholder='Title' onChange={e => setTitle(e.target.value)} value={title}></input>
     <textarea className={styles.content} placeholder='Content' onChange={e => setContent(e.target.value)} value={content}></textarea> 
     <div className={styles.note_buttons}>
-    <button onClick={() => save(idx, {title, content, inEdit: false})}>Save</button>
+    <button /* onClick={() => save(idx, {title, content, inEdit: false})} */>Save</button>
     </div>
   </div>
   )
 }
 
 export function App() {
-  const localNotesRaw = localStorage.getItem("notes")
+  /* const localNotesRaw = localStorage.getItem("notes")
   const localNotes: Note[] = localNotesRaw ? JSON.parse(localNotesRaw) : [];
-  const [notes, setNotes] = useState(localNotes);
+  const [notes, setNotes] = useState(localNotes); */
   const [inputTitle, setInputTitle] = useState("");
   const [inputContent, setInputContent] = useState("");
   
-  useEffect(() => {
+/*   useEffect(() => {
     localStorage.setItem("notes", JSON.stringify(notes));
-  }, [notes]);
+  }, [notes]); */
   
-  const deleteNote = (i : number) => {
+/*   const deleteNote = (i : number) => {
     const newNotes = [...notes]
     newNotes.splice(i,1);
     setNotes(newNotes);
@@ -75,12 +75,12 @@ export function App() {
     const newNotes = [...notes]
     newNotes.splice(i,1,n);
     setNotes(newNotes);
-  }
+  } */
   return (
     <div className={styles.main}>
       <h1> Notes App</h1>
       <div className={styles.notes}>
-      {notes.length ?
+      {/* {notes.length ?
         notes.map((note: Note, i: number) => 
           note.inEdit 
           ? <EditableNote note={note} idx={i} save={saveNote} /> 
@@ -88,13 +88,13 @@ export function App() {
         
         )
       : <h2>No Notes left</h2>
-      }
+      } */}
       </div>
       <div className={styles.add_note}>
         <input className={styles.title} placeholder='Title' onChange={e => setInputTitle(e.target.value)} value={inputTitle}></input>
         <textarea className={styles.content} placeholder='Content' onChange={e => setInputContent(e.target.value)} value={inputContent}></textarea>
         <div>
-          <button onClick={
+          <button /*onClick={
             () => {
                 if (!inputTitle || !inputContent) {
                   alert("Put in a title and content for the note!")
@@ -104,7 +104,7 @@ export function App() {
                 setInputTitle("")
                 setInputContent("")
               }
-            }>
+            }*/>
             Add Note
             </button>
       </div>
